1e78b18781a956b2b860ab3350853e1649ce4033
Charlotte Desvages <charlotte.desvages@ed.ac.uk>
2023-01-30 13:06:54 +0000

    Solutions released

---
+++ week01_solutions.py
@@ -0,0 +1,26 @@
+# Part 1
+name = 'Josephine'
+age = 36
+birthday = '3rd May'
+
+print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'My birthday is on the {birthday}.')
+
+
+# Part 2: after switching roles
+print(f'On the {birthday}, I will be {age + 1} years old.')
+
+
+# Part 3: bonus questions
+name_1 = 'Jerry'
+age_1 = 20
+birthday_1 = '24th June'
+
+name_2 = 'Tom'
+age_2 = 44
+birthday_2 = '11th January'
+
+print(f'{name_1} is {age_1}, and their birthday is on the {birthday_1}.')
+print(f'{name_2} is {age_2}, and their birthday is on the {birthday_2}.')
+
+print(f'Is {name_1} younger than {name_2}? {age_1 < age_2}')
